2c8499e40997f65d863cdc59664e1d2d593e4907
Ionardo <ionardo@mail.ru>
2022-06-08 16:50:33 +0300

    Добавлен класс converter.

--- Praktikum.java
+++ Praktikum.java
@@ -1,3 +1,5 @@
-public static void main(String[] args) {
-	System.out.println("привет мир!");
+public class Converter{
+	public static void main(String[] args) {
+		System.out.println("привет мир!");
+	}
 }
